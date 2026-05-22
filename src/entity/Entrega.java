ac57318e5acc54aa36e4102c7711937ad4abf446
gustavo_richardt <gustavo_richardt@estudante.sesisenai.org.br>
2026-05-22 19:03:08 -0300

    feat(entrega): cria classe Entrega

---
+++ src/entity/Entrega.java
@@ -0,0 +1,68 @@
+package entity;
+
+public class Entrega {
+
+    int id;
+    String cliente;
+    String entregador;
+    String descricao;
+    String status;
+
+    public Entrega(int id, String cliente, String entregador, String descricao, String status) {
+        this.id = id;
+        this.cliente = cliente;
+        this.entregador = entregador;
+        this.descricao = descricao;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getEntregador() {
+        return entregador;
+    }
+
+    public void setEntregador(String entregador) {
+        this.entregador = entregador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void exibirDados() {
+
+        System.out.println("Id: " + id);
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Entregador: " + entregador);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Status: " + status);
+
+    }
+}
