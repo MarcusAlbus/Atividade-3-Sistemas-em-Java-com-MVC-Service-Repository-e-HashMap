123275906add2a77cb3a15014f6214c6f7f49ad2
marcus_arruee <marcus_arruee@estudante.sesisenai.org.br>
2026-05-29 20:09:22 -0300

    feat(entrega):Arrumando erros do Entrega

--- src/model/entity/Entrega.java
+++ src/model/entity/Entrega.java
@@ -24,19 +24,19 @@ public class Entrega {
         this.id = id;
     }
 
-    public String getCliente() {
+    public Cliente getCliente() {
         return cliente;
     }
 
-    public void setCliente(String cliente) {
+    public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
 
-    public String getEntregador() {
+    public Entregador getEntregador() {
         return entregador;
     }
 
-    public void setEntregador(String entregador) {
+    public void setEntregador(Entregador entregador) {
         this.entregador = entregador;
     }
 
